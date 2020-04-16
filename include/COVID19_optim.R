ce7e453dc8478cab3455ed48764c4278587f66bf
Maxime Oriol <maxoriol94@gmail.com>
2020-04-16 23:21:17 +0200

    Paramatrezation of the pace
    It enables to choose if solver searches closer or farther from the starting point

--- include/COVID19_optim.R
+++ include/COVID19_optim.R
@@ -3,26 +3,30 @@ source('include/COVID19_optim_functions.R')
 #------------------------------------------------------------------------
 # 0.0 - STATE OF THE ART
 #------------------------------------------------------------------------
+OPT <- list()
 
-L0 <- LOSS_F(
+OPT$L0 <- LOSS_F(
   y=D$KPI[!D$KPI$cumul,c('dt', 'kpi', 'qt_min')],
   idg=INIT_IDG(D$dFR, H$init),
-  tm=H$all, sr=H$sr, cdday=H$cdday
+  sce=S$sce, sr=H$sr, pdday=H$pdday
 )
 
-OPT <- list()
+
 #------------------------------------------------------------------------
 # 1.0 - ESTIMATION INITIALISATION
 #------------------------------------------------------------------------
 
 OPT$init <- OPTIMIZE_PARAM(
-  theta='init', nmax=100, eps=100, p=100, target=L0,
+  theta='init', nmax=10, eps=100, nb=10, pace=10, target=OPT$L0,
   init=H$init,
   y=D$KPI[!D$KPI$cumul,c('dt', 'kpi', 'qt_min')],
-  demo=D$dFR, tm=H$all, sr=H$sr, cdday=H$cdday)
-OPT$init <- OPT$init[order(OPT$init), ]
+  demo=D$dFR, sce=S$sce, sr=H$sr, pdday=H$pdday)
+OPT$init <- OPT$init[order(OPT$init$loss, na.last=NA), ]
 head(OPT$init)
 tail(OPT$init)
+#OPT$init <- read.csv(file='data/OPTI_init.csv', sep=',', dec='.')
+OPT$graph_init <- aggregate(data=OPT$init, loss~dt+age, FUN=median)
+ggplot(data=OPT$graph_init, aes(x=age, y=dt))+geom_tile(aes(fill=loss), colour='white')+scale_fill_gradient(low='green', high='red')+ggtitle('Loss function by date and age')
 #write.csv(x=data.frame(dt_cal=Sys.Date(), OPT$init), file='data/OPTI_init.csv', quote=F, append=T, row.names=F)
 
 
@@ -30,13 +34,32 @@ tail(OPT$init)
 # 2.0 - ESTIMATION TRANSMISSION MATRIX (WORK iN PROGRESS)
 #------------------------------------------------------------------------
 OPT$tm <- OPTIMIZE_PARAM(
-  theta='tm', nmax=5, eps=100, p=20, target=L0,
+  theta='tm', nmax=10, eps=100, nb=10, pace=10, target=OPT$L0,
   init=H$init,
   y=D$KPI[!D$KPI$cumul,c('dt', 'kpi', 'qt_min')],
-  demo=D$dFR, tm=H$all, sr=H$sr, cdday=H$cdday)
-
+  demo=D$dFR, sce=S$sce, sr=H$sr, pdday=H$pdday
+  )
+ 
 OPT$tm<- OPT$tm[order(OPT$tm$loss), ]
 head(OPT$tm)
 tail(OPT$tm)
+ggplot(data=OPT$tm, aes(x=b0, y=b1, color=loss)+geom_point(shape=19)+scale_fill_gradient(low='green', high='red')+ggtitle('Loss function by coefficient')
 #write.csv(x=data.frame(dt_cal=Sys.Date(), OPT$tm), file='data/OPTI_TM.csv', quote=F, append=T, row.names=F)
+
+#------------------------------------------------------------------------
+# 3.0 - ESTIMATION of DISEASE DAY PARAMETERS
+#------------------------------------------------------------------------
+OPT$pdday <- OPTIMIZE_PARAM(
+  theta='pdday', nmax=10, eps=100, nb=10, pace=10, target=OPT$L0,
+  init=H$init,
+  y=D$KPI[!D$KPI$cumul,c('dt', 'kpi', 'qt_min')],
+  demo=D$dFR, sce=S$sce, sr=H$sr, pdday=H$pdday)
+
+OPT$pdday <- OPT$pdday[order(OPT$pdday$loss), ]
+head(OPT$pdday)
+tail(OPT$pdday)
+test <- GET_DDAY(OPT$pdday[1, ])
+plot(test$dday, test$pdeath, type='l', col='blue', ylim=c(0,1))
+lines(test$dday, test$ctrans, col='green')
+#write.csv(x=data.frame(dt_cal=Sys.Date(), OPT$pdday), file='data/OPTI_PDDAY.csv', quote=F, append=T, row.names=F)
 
--- include/COVID19_optim_functions.R
+++ include/COVID19_optim_functions.R
@@ -1,6 +1,6 @@
-#y=D$KPI[!D$KPI$cumul,c('dt', 'kpi', 'qt_min')]; idg=IDG; tm=H$all; sr=H$sr; cdday=H$cdday
-LOSS_F <- function(y, idg, tm, sr, cdday){
-  y_ <- SIMU_PERIOD(dt_end=max(y$dt), idg=idg, tm=tm, sr=sr, cdday=cdday)
+#y=D$KPI[!D$KPI$cumul,c('dt', 'kpi', 'qt_min')]; idg=IDG; sce=D$sce; sr=H$sr; pdday=H$pdday
+LOSS_F <- function(y, idg, sce, sr, pdday){
+  y_ <- SIMU_PERIOD(dt_end=max(y$dt), idg=idg, sce=sce, sr=sr, pdday=pdday)
   y_$kpi <- NA
   y_$kpi[y_$liv&y_$dday==1] <- 'infected'
   y_$kpi[y_$liv&y_$dday==(H$ddaymax+1)] <- 'immunized'
@@ -14,21 +14,28 @@ LOSS_F <- function(y, idg, tm, sr, cdday){
   return(sum(y$cost))
 }
 
-OPTIMIZE_PARAM <- function(theta, nmax, eps, p, target, init, y, demo, tm, sr, cdday){
+# theta='init';nmax=5;eps=100;nb=5;target=OPT$L0;init=H$init;y=D$KPI[!D$KPI$cumul,c('dt', 'kpi', 'qt_min')];demo=D$dFR;sce=S$sce;sr=H$sr;pdday=H$pdday
+OPTIMIZE_PARAM <- function(theta, nmax, eps, nb, pace, target, init, y, demo, sce, sr, pdday){
   if(nmax>0){
     if(theta=='init'){
-      theta_ <- MOVE_INIT(p, init=init, demo=demo, as.list=T)
+      theta_ <- MOVE_INIT(n=nb, init=init, demo=demo, pace=pace,  as.list=T)
       LOSS <- lapply(
         X= theta_,
-        FUN=function(a) data.frame(a, loss=LOSS_F(y=y, idg=INIT_IDG(demo, a), tm=tm, sr=sr, cdday=cdday))
+        FUN=function(a) data.frame(a, loss=LOSS_F(y=y, idg=INIT_IDG(demo, a), sce=sce, sr=sr, pdday=pdday))
       )
     } else if(theta=='tm') {
-      theta_ <- MOVE_TM(p, tm=tm)
+      theta_ <- MOVE_TM(n=nb, sce=sce, pace=pace)
       LOSS <- lapply(
         X=theta_,
-        FUN=function(a) data.frame(a$coef, loss=LOSS_F(y=y, idg=INIT_IDG(demo = demo, init=init), tm=a$tab, sr=sr, cdday=cdday))
+        FUN=function(a) data.frame(a$coef, loss=LOSS_F(y=y, idg=INIT_IDG(demo = demo, init=init), sce=a$tab, sr=sr, pdday=pdday))
       )
-    } else{
+    } else if(theta=='pdday'){
+      theta_ <- MOVE_PDDAY(n=nb, pdday=pdday, pace=pace, as.list=T)
+      LOSS <- lapply(
+        X=theta_,
+        FUN=function(a) data.frame(a[1,], loss=LOSS_F(y=y, idg=INIT_IDG(demo = demo, init=init), sce=sce, sr=sr, pdday=a))
+      )
+    } else {
       return(NULL)
     }
     LOSS <- do.call(rbind, LOSS)
@@ -37,8 +44,8 @@ OPTIMIZE_PARAM <- function(theta, nmax, eps, p, target, init, y, demo, tm, sr, c
     # If not, we return the result
     if((newloss-eps)<target){
       if(theta=='init') init <- theta_[[which.min(LOSS$loss)]]
-      if(theta=='tm') tm <- theta_[[which.min(LOSS$loss)]]$tab
-      NEXT_LOSS=OPTIMIZE_PARAM(theta=theta, nmax=nmax-1, eps=eps, p=p, target=newloss, init=init, y=y, demo=demo, tm=tm, sr=sr, cdday=cdday)
+      if(theta=='sce') sce <- theta_[[which.min(LOSS$loss)]]$tab
+      NEXT_LOSS=OPTIMIZE_PARAM(theta=theta, nmax=nmax-1, eps=eps, nb=nb, pace=pace, target=newloss, init=init, y=y, demo=demo, sce=sce, sr=sr, pdday=pdday)
       return(rbind(LOSS, NEXT_LOSS))
     } else {
       return(LOSS)
@@ -53,15 +60,15 @@ OPTIMIZE_PARAM <- function(theta, nmax, eps, p, target, init, y, demo, tm, sr, c
 # 1.0 - MOVING FUNCTION
 #------------------------------------------------------------------------
 
-MOVE_INIT <- function(n, init, demo, as.list=F){
+MOVE_INIT <- function(n, init, demo, pace=0.5, as.list=F){
   nth <- runif(n)
-  nth <- rgeom(n, 0.5)*(-1*(nth<0.5)+1*(nth>=0.5))
+  nth <- rgeom(n, 1/pace)*(-1*(nth<0.5)+1*(nth>=0.5))
   out <- data.frame(
     liv=T,
     dt=min(init$dt)+nth,
     age=sample(unique(demo$age), n, replace=T),
-    dday=1+rgeom(n, 0.5),
-    qt=1+rgeom(n,0.5)
+    dday=1,
+    qt=1
   )
   out <- out[!duplicated(out),]
   return(if(as.list) split(out, 1:dim(out)[1]) else out)
@@ -69,9 +76,17 @@ MOVE_INIT <- function(n, init, demo, as.list=F){
 
 # Move transmisson number matrix by age
 
-MOVE_TM <- function(n, tm, max=2){
-  out <- data.frame(b0=pmax(min(tm$ntrans), runif(n,0,max)), b1=runif(n,0,max), b2=runif(n,0,max))
+MOVE_TM <- function(n, sce, pace=0.5){
+  out <- data.frame(b0=runif(n,min = 1-pace/2,max=1+pace/2), b1=runif(n,min = 1-pace/2,max=1+pace/2))
   out <- split(out, 1:dim(out)[1])
-  out <- lapply(out, function(a) list(coef=a, tab=data.frame(tm[,c('agent', 'target')], ntrans=a$b0+a$b1*tm$ntrans+a$b2*tm$ntrans^2)))
+  out <- lapply(out, function(a) list(coef=a, tab=data.frame(sce[,c('dt', 'agent', 'target')], ntrans=exp(a$b0+a$b1*sce$ntrans))))
   return(out)
+}
+
+# Move transmission coefficient and death probability by day during
+
+MOVE_PDDAY <- function(n, pdday, pace=1, as.list=F){
+  out <- sapply(c(pdday$shape[1], pdday$scale[1]), function(a) pmax(0,a+runif(n=n, -pace, pace)))
+  out <- data.frame(pdday='weibull', shape=out[,1], scale=out[,2])
+  return(if(as.list) split(out, 1:dim(out)[1]) else out)
 }
